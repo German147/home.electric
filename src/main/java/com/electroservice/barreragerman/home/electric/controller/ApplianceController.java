c2b6f32ed159b6b259f8f613cbc1efb34f0b2db1
Germán <barreragerman27@gmail.com>
2021-11-16 15:06:48 -0300

    It is instantiated a GetMapping at the controller

--- src/main/java/com/electroservice/barreragerman/home/electric/controller/ApplianceController.java
+++ src/main/java/com/electroservice/barreragerman/home/electric/controller/ApplianceController.java
@@ -3,20 +3,20 @@ package com.electroservice.barreragerman.home.electric.controller;
 import com.electroservice.barreragerman.home.electric.model.Appliance;
 import com.electroservice.barreragerman.home.electric.repository.AppliancesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/appliance")
+@RequestMapping("/home_electric")
 public class ApplianceController {
 
     @Autowired
     private AppliancesRepository appliancesRepository;
 
     @PostMapping("/postAppliance")
-    public Appliance addAppliances(@RequestBody Appliance newAppliance){
+    public Appliance addAppliances(@RequestBody Appliance newAppliance) {
         Appliance appl = new Appliance(
                 newAppliance.getIdCode(),
                 newAppliance.getName(),
@@ -28,5 +28,20 @@ public class ApplianceController {
 
         return appl;
     }
+
+    @GetMapping("/appliances")
+    public List<Appliance> getAppliances() {
+        List<Appliance> applianceList = appliancesRepository.findAll();
+        return applianceList;
+    }
+
+    @GetMapping("/appliance/{idCode}")
+    public Optional<Appliance> getAppliance(@PathVariable Integer idCode){
+        Optional<Appliance> app = appliancesRepository.findById(idCode);
+        return  app;
+    }
+
+
+
 
 }
